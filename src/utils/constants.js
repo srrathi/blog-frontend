efc2ed85949c4fa7d8108147ac549361cf1ced4b
srrathi <srrathi2000@gmail.com>
2022-01-23 03:18:29 +0530

    changed api base url for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = "http://localhost:9002/api";
+// FOR LOCAL DEVELOPMENT
+// export const API_BASE_URL = "http://localhost:9002/api";
+
+// FOR PRODUCTION
+export const API_BASE_URL = "https://srrathi-blog-backend.herokuapp.com/api"
