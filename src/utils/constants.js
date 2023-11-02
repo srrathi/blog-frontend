02ffcad60aa890cac7952860428e3c31dc1c2623
Sitaram Rathi <srrathi2000@gmail.com>
2023-11-02 23:47:56 +0530

    updated backend api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@
 // export const API_BASE_URL = "http://localhost:9002/api";
 
 // FOR PRODUCTION
-export const API_BASE_URL = "https://srrathi-blog-backend.herokuapp.com/api"
+export const API_BASE_URL = "https://blog-backend-lj4c.onrender.com/api"
